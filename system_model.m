be4c81f2518e7e69f11119a152deb73c0b3a65bd
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-03-13 16:02:42 +0100

    added comparison output

--- system_model.m
+++ system_model.m
@@ -8,15 +8,15 @@ err = 1/100; % steady state error
 zeta= 0.4559; 
 Wn = 4.6/zeta; 
 s = tf('s'); % s-doamin
-G = (1 / (s^2 + 2*Wn*zeta*s + Wn^2)) * (0.21 / (s^2));
+G = (1 / (s^2 + 2*Wn*zeta*s + Wn^2)) * (0.21 / (s^2)); % open loop TF
 
 
 
-
-
-
-
-
+% using P,PI,PD,PID 
+u_P = P(e_p, p_p, i_p, d_p, Kp, Ki, Kd);
+u_PI = PI(e_pi, p_pi, i_pi, d_pi, Kp, Ki, Kd);
+u_PD = PD(e_pd, p_pd, i_pd, d_pd, Kp, Ki, Kd);
+u_PID = PIDController(e_pid, p_pid, i_pid, d_pid);
 
 
 
